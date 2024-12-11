d8f37eaaf76a83f752ea78ea93a338ab1b0641a1
Encephala <jonathan_rietveld@hotmail.com>
2024-12-11 18:49:35 +0100

    Add day template

---
+++ src/bin/day-xx/main.rs
@@ -0,0 +1,21 @@
+fn part_1(input: &str) -> usize {
+    todo!();
+}
+
+fn part_2(input: &str) -> usize {
+    todo!();
+}
+
+fn main() {
+    let test_input = "";
+
+    let actual_input = &std::fs::read_to_string("src/bin/day-xx/input.txt").unwrap();
+
+    let result_1 = part_1(test_input);
+
+    println!("Part 1: {result_1}");
+
+    let result_2 = part_2(test_input);
+
+    println!("Part 2: {result_2}");
+}
